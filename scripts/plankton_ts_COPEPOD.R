42101f17387b69aa6756f5035dfc13c9d039a531
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2021-03-03 18:18:57 -0600

    update plankton data (only goes through 1993?)

--- scripts/plankton_ts_COPEPOD.R
+++ scripts/plankton_ts_COPEPOD.R
@@ -7,7 +7,7 @@
 library(tidyverse)
 
 # make list of file names
-myfiles <- list.files("plankton_ts_GOM/data_src/short-format/", 
+myfiles <- list.files("data/raw_data/plankton_ts_GOM/data_src/short-format/", 
                       full.names = TRUE, pattern = "*.csv")
 
 # empty list to put each df into
@@ -16,7 +16,7 @@ df_list <- list()
 # read in each file, remove first 15 lines and blank row after column names
 # then place in df_list
 for (i in 1:length(myfiles)) {
-  file <- read.csv("plankton_ts_GOM/data_src/short-format/zzzz-610716_us-05103.csv", 
+  file <- read.csv(myfiles[i], 
                    skip = 15)
   df_list[[i]] <- file[-1,]
 }
@@ -25,4 +25,4 @@ for (i in 1:length(myfiles)) {
 plankton_data <- do.call(rbind, df_list) 
 
 # write big df to csv file
-write_csv(plankton_data, "prepped_data/plankton_data.csv")
+write_csv(plankton_data, "data/raw_data/plankton_ts_GOM/plankton_data.csv")
